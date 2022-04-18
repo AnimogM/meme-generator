1cad1bb2e3b784b95129d2a22d8f248c815bae27
Gomina Maryam <80972735+AnimogM@users.noreply.github.com>
2022-04-18 10:01:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,9 +64,9 @@ def meme_post():
     image_url = request.form['image_url']
     body = request.form['body']
     author = request.form['author']
-    img = requests.get(image_url)
     path = None
     try:
+        img = requests.get(image_url)
         img_files = f'tmp/{random.randint(0, 100000000)}.jpg'
         open(img_files, 'wb').write(img.content)
     except:
